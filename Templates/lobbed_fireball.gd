5cae07a1aa2cdf3179d74e45b306ecd4b21a5978
Morgan  Brubaker <43830512+mzbrubak@users.noreply.github.com>
2025-01-24 11:00:08 -0800

    Lobbed Fireball has AoE indicator on hit

--- Templates/lobbed_fireball.gd
+++ Templates/lobbed_fireball.gd
@@ -9,7 +9,11 @@ func fire( facing:Vector3, speed:float ) -> void:
 func _on_body_entered(body):
     if body.has_method("takeDamage"):
         body.takeDamage(directdamage);
+    $"SplashDamageArea/AoE Image".visible=true
+    $"Direct Image".visible=false
+    self.freeze=true
     for splashedbody in $SplashDamageArea.get_overlapping_bodies():
         if splashedbody!=body and splashedbody.has_method("takeDamage"):
             splashedbody.takeDamage(splashdamage)
+    await get_tree().create_timer(0.5).timeout
     self.queue_free()
--- enemy_base.gd
+++ enemy_base.gd
@@ -1,14 +1,16 @@
 class_name EnemyBase extends Actor
 var health:int = 10
+var dying:bool = false
 
 func takeDamage(damage:int)->void:
     $AudioStreamPlayer3D.play()
     health-=damage
-    if health<=0:
+    if health<=0 and !dying:
         prepareToDie()
 
 func prepareToDie()->void:
     #allow enemy to say their last words before deletion
+    dying=true
     $AudioStreamPlayer3D.finished.connect(die)
     
 func die():
