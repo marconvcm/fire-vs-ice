367850619ed5ea3dc53370b327a30d2ffd3eca9b
Morgan  Brubaker <43830512+mzbrubak@users.noreply.github.com>
2025-01-29 22:12:13 -0800

    Mimics stay a fixed distance away from the player
    Player has a second hitbox that specifically keeps mimics a fixed distance away.  The player can push mimics using the hitbox but the mimics are fast enough that it will look like they are staying away on purpose.  Mimics currently won't spread away from each other to flank the player but current behavior might be fine

--- Templates/mimic.gd
+++ Templates/mimic.gd
@@ -11,10 +11,9 @@ var target_position=null
 #states: 0=hiding, 1=activated/chasing, 2=looking for player, 3=returning to hiding spot
 func _ready():
     $GiveUpTimer.timeout.connect(giveup)
-    $ExplodeTimer.timeout.connect(explode)
     navi.velocity_computed.connect(Callable(_on_velocity_computed))
     health=5
-    speed=2
+    speed=50
 
 func _process(_delta):
     match state:
@@ -39,7 +38,7 @@ func _process(_delta):
 func _physics_process(_delta):
     if target!=null and (!state==0):
         var next_path_position: Vector3 = navi.get_next_path_position()
-        var new_velocity: Vector3 = global_position.direction_to(next_path_position)*speed
+        var new_velocity: Vector3 = global_position.direction_to(next_path_position)*speed+get_gravity_strength()
         _on_velocity_computed(new_velocity)
 
 func _on_activation_area_body_shape_entered(_body_rid, collidingbody, _body_shape_index, local_shape_index):
@@ -70,13 +69,6 @@ func state_transition(newstate:int):
 func is_target_visible():
     $RayCast3D.target_position=target.global_position-self.global_position
     return $RayCast3D.get_collider()==target
-    
-func prepareToDie():
-    dying=true
-    $ExplodeTimer.start()
-
-func explode():
-    pass
 
 func begin_hiding():
     pass
