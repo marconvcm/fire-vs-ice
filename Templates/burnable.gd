58c428bdfd0f9be6ce3fc421e2c06c649d57a6a4
Morgan  Brubaker <43830512+mzbrubak@users.noreply.github.com>
2025-02-05 13:34:27 -0800

    Burnable template object for testing, plus arrow sprite import and sounds

---
+++ Templates/burnable.gd
@@ -0,0 +1,14 @@
+class_name burnable extends MeshInstance3D
+
+func burn():
+    play_burning_animation()
+    disable()
+    
+func play_burning_animation():
+    pass
+    
+func restore():
+    pass
+    
+func disable():
+    pass
