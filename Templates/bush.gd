621f7ce44a9c0532e3ff6608df58a67c6c1d5627
Morgan  Brubaker <43830512+mzbrubak@users.noreply.github.com>
2025-02-17 21:13:35 -0800

    Bush sprite
    Bushes have 2D sprites that always face the camera.  Currently only using the first sprite, can change easily later.  Using bush layout from Feb 6 map, may place more manually for design/balance

--- Templates/bush.gd
+++ Templates/bush.gd
@@ -1,5 +1,7 @@
 extends burnable
 @export var spriteindex=1
+#Note: this script should be attached to the sphere mesh that 
 
 func _ready():
+    self
     $StaticBody3D/AnimatedSprite3D.set_animation(str(spriteindex))
