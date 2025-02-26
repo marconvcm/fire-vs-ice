fa3b55a2890a466272fc8a8175379ffbf18e8470
Morgan  Brubaker <43830512+mzbrubak@users.noreply.github.com>
2025-02-26 14:29:30 -0800

    Torches can be lit
    When hit, torches will display a flame sprite and emit a (currently very dim) light.  Reusing fireball sprite for flames

--- Templates/bush.gd
+++ Templates/bush.gd
@@ -1,10 +1,29 @@
 extends burnable
 @export var spriteindex=1
+@onready var sprite=$StaticBody3D/AnimatedSprite3D
 #Note: this script should be attached to the sphere mesh that 
 
 func _ready():
-    $StaticBody3D/AnimatedSprite3D.set_animation(str(spriteindex))
+    sprite.set_animation(str(spriteindex))
 
+func burn(touching:bool)->float:
+    play_burning_animation()
+    #disable()
+    if touching: 
+        return heatvalue
+    else:
+        return 0#might let it reduce heat loss over time or something
+
+#turns off collision
+func disable():
+    hitbox.set_collision_layer_value(6,false)
+    pass
+
+#turns collision back on after a reload
 func restore():
-    #has it's own version to explicitly not make itself visible on restoration
     hitbox.set_collision_layer_value(6,true)
+
+func play_burning_animation():
+    sprite.set_animation("burning")
+    sprite.play()
+    pass
---
+++ Templates/torch.gd
@@ -0,0 +1,15 @@
+extends burnable
+@export var torch_ID:int
+@onready var firesprite=$StaticBody3D/FireSprite
+
+func ready():
+    firesprite.visible=false
+
+func burn(touching:bool) -> float:
+    if firesprite.visible==false:
+        firesprite.visible=true
+    print('this is happening')
+    return 0
+
+func restore():
+    pass #do nothing, overriding default, since resets shouldn't effect torch state
